061bf239b29fb8bbabc84a57043514601d027e46
Hamza Sayed-Ali <hamzasayedali@gmail.com>
2019-08-14 22:27:40 -0400

    updated contact php

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <hamzasayedali@gmail.com>';
+$from = 'Demo contact form <contact@organieco.ca>';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'Demo contact form <hamzasayedali@gmail.com>';
